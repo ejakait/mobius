5c4776887ec926d2de1805524abfb92dc6ef63aa
ejakait <ejakaitcaleb@gmail.com>
2017-11-02 10:11:44 +0300

    formating

--- mobius_api/funds/models.py
+++ mobius_api/funds/models.py
@@ -52,16 +52,18 @@ class FundScheme(models.Model):
     aum_date = models.DateField(
         auto_now=False, auto_now_add=False, blank=True, null=True)
 
+
 class SchemePlan(models.Model):
     '''
     Stores the details of a scheme. every FundScheme has multiple SchemePlans
     Like Mirae Asset Emerging Bluechip Fund Direct Growth, Mirae Asset Emerging Bluechip Fund Direct Dividend, 
         Mirae Asset Emerging Bluechip Fund Regular Growth, Mirae Asset Emerging Bluechip Fund Regular Dividend etc
     '''
-    name = models.CharField(max_length=100, unique=True)  
-    fund_scheme = models.ForeignKey(FundScheme, on_delete=models.PROTECT, null=True, related_name="scheme_plan_list")
-    
-    # imp codes for doing transaction 
+    name = models.CharField(max_length=100, unique=True)
+    fund_scheme = models.ForeignKey(
+        FundScheme, on_delete=models.PROTECT, null=True, related_name="scheme_plan_list")
+
+    # imp codes for doing transaction
     bse_code = models.CharField(max_length=15, blank=True)
     rta_code = models.CharField(max_length=10, blank=True)
     amc_code = models.CharField(max_length=10, blank=True)
@@ -69,21 +71,27 @@ class SchemePlan(models.Model):
 
     # schemeplan type; figured out by parsing name of SchemePlan
     # regular, growth is most commonly transacted
-    ## set true if 'direct' in name; false means regular
+    # set true if 'direct' in name; false means regular
     if_direct = models.NullBooleanField(default=False, null=True)
-    ## set false for dividend i.e. if 'dividend' in name but 'dividend yield' not in name
+    # set false for dividend i.e. if 'dividend' in name but 'dividend yield' not in name
     if_growth = models.NullBooleanField(default=False, null=True)
     if_open = models.NullBooleanField(default=True, null=True)
 
     # key details
-    launch_date = models.DateField(auto_now=False, auto_now_add=False, blank=True, null=True)
-    asset_size = models.FloatField(blank=True, null=True)  # stale data- of direct plan  # in Rs cr
-    asset_size_date = models.DateField(auto_now=False, auto_now_add=False, blank=True, null=True)
-    expense_ratio = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(100)], blank=True, null=True)  # in %
-    expense_ratio_date = models.DateField(auto_now=False, auto_now_add=False, blank=True, null=True)
+    launch_date = models.DateField(
+        auto_now=False, auto_now_add=False, blank=True, null=True)
+    # stale data- of direct plan  # in Rs cr
+    asset_size = models.FloatField(blank=True, null=True)
+    asset_size_date = models.DateField(
+        auto_now=False, auto_now_add=False, blank=True, null=True)
+    expense_ratio = models.FloatField(validators=[MinValueValidator(
+        0), MaxValueValidator(100)], blank=True, null=True)  # in %
+    expense_ratio_date = models.DateField(
+        auto_now=False, auto_now_add=False, blank=True, null=True)
     nav_latest = models.FloatField(blank=True, null=True)
-    nav_latest_date = models.DateField(auto_now=False, auto_now_add=False, blank=True, null=True)
-    
+    nav_latest_date = models.DateField(
+        auto_now=False, auto_now_add=False, blank=True, null=True)
+
     # dividend details
     dividend_history = models.CharField(max_length=1000, blank=True)
 
@@ -93,11 +101,14 @@ class AssetPortfolio(models.Model):
     Stores overall portoflio details for a fund scheme or benchmark index or fund category. 
     in every row, any one foreign key is filled while the other two are empty
     '''
-    fund_scheme = models.ForeignKey(FundScheme, on_delete=models.PROTECT, blank=True, null=True)
-    fund_category = models.ForeignKey(FundCategory, on_delete=models.PROTECT, blank=True, null=True)
-    
+    fund_scheme = models.ForeignKey(
+        FundScheme, on_delete=models.PROTECT, blank=True, null=True)
+    fund_category = models.ForeignKey(
+        FundCategory, on_delete=models.PROTECT, blank=True, null=True)
+
     # composition of portfolio
-    num_assets = models.IntegerField(validators=[MinValueValidator(0)], blank=True, null=True)
+    num_assets = models.IntegerField(
+        validators=[MinValueValidator(0)], blank=True, null=True)
     debt_pc = models.FloatField(default=0)
     equity_pc = models.FloatField(default=0)
     cash_pc = models.FloatField(default=0)
